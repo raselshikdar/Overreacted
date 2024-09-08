ee21cf83c9987ae5d261820eb89d37013f3dc9ca
Rasel Shikdar <103440692+raselshikdar@users.noreply.github.com>
2024-09-08 19:30:14 +0600

    Update page.js

--- app/[slug]/page.js
+++ app/[slug]/page.js
@@ -74,6 +74,24 @@ export default async function PostPage({ params }) {
           }}
         />
           <hr />
+          <p>
+          <script src="https://giscus.app/client.js"
+        data-repo="raselshikdar/blog"
+        data-repo-id="R_kgDOMuevqw"
+        data-category="General"
+        data-category-id="DIC_kwDOMuevq84CiS_S"
+        data-mapping="pathname"
+        data-strict="0"
+        data-reactions-enabled="1"
+        data-emit-metadata="0"
+        data-input-position="top"
+        data-theme="preferred_color_scheme"
+        data-lang="en"
+        crossorigin="anonymous"
+        async>
+</script>
+          </p>
+          <hr />
         <p>
           <Link href={discussUrl}>Discuss on GitHub Issues</Link>
           &nbsp;&nbsp;&middot;&nbsp;&nbsp;
