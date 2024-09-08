3def0d95d21baa05e4c77ac5e773f8e2dac9f045
Rasel Shikdar <103440692+raselshikdar@users.noreply.github.com>
2024-09-08 15:31:17 +0600

    Update HomeLink.js

--- app/HomeLink.js
+++ app/HomeLink.js
@@ -28,7 +28,7 @@ export default function HomeLink() {
           transition: "--myColor1 0.2s ease-out, --myColor2 0.2s ease-in-out",
         }}
       >
-        overreacted
+        Overreacted (Rasel Shikdar's Blog)
       </span>
     </Link>
   );
