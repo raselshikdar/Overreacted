fab7029963b9e1b390bea61c26e2db36449d8271
Rasel Shikdar <103440692+raselshikdar@users.noreply.github.com>
2024-09-08 19:34:00 +0600

    Update page.js

--- app/[slug]/page.js
+++ app/[slug]/page.js
@@ -25,9 +25,9 @@ export default async function PostPage({ params }) {
     }
   }
   const { content, data } = matter(file);
-  const discussUrl = `https://github.com/raselshikdar/blog/discussions/main/public/${encodeURIComponent(
-    params.slug,
-  )}/index.md`;
+  const discussUrl = `https://x.com/search?q=${encodeURIComponent(
+    `https://blog.rasel.us.kg/${params.slug}/`,
+  )}`;
   const editUrl = `https://github.com/raselshikdar/blog/edit/main/public/${encodeURIComponent(
     params.slug,
   )}/index.md`;
