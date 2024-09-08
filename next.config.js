cf3cd129db5328917fff998ee624c73a26e64831
Rasel Shikdar <103440692+raselshikdar@users.noreply.github.com>
2024-09-08 19:09:02 +0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,3 +2,14 @@ module.exports = {
   output: "export",
   trailingSlash: true,
 };
+
+const nextConfig = {
+	env: {
+		COMMENTS_REPO: 'ayoubkhial/nextjs-giscus-blog',
+		COMMENTS_REPO_ID: 'R_kgDOJlwoBA',
+		COMMENTS_CATEGORY: 'Announcements',
+		COMMENT_CATEGORY_ID: 'DIC_kwDOJlwoBM4CWpCu',
+	},
+};
+ 
+module.exports = nextConfig;
