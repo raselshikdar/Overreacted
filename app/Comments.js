295368c4b09c0d0fd1b20a11ef34005c78b167fb
Rasel Shikdar <103440692+raselshikdar@users.noreply.github.com>
2024-09-08 19:06:55 +0600

    Create Comments.js

---
+++ app/Comments.js
@@ -0,0 +1,17 @@
+<section class="giscus"></section>
+
+<script src="https://giscus.app/client.js"
+        data-repo="raselshikdar/blog"
+        data-repo-id="R_kgDOMuevqw"
+        data-category="General"
+        data-category-id="DIC_kwDOMuevq84CiS_S"
+        data-mapping="pathname"
+        data-strict="0"
+        data-reactions-enabled="1"
+        data-emit-metadata="0"
+        data-input-position="top"
+        data-theme="preferred_color_scheme"
+        data-lang="en"
+        crossorigin="anonymous"
+        async>
+</script>
